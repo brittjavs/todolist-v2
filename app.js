804776b99f91b901da7af04053246560c1f37e8a
Brittany Javalera <brittjavalera@gmail.com>
2021-03-28 15:11:13 -0700

    make deleting dynamic by adding hidden input to checkbox that can be used to get listName in delete method. add lodash and requrire it so our list titles are capitalized

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 require('dotenv').config()
 
 const mongoose = require('mongoose');
+const _ = require('lodash')
 
 const uri = process.env.DB_URI
 
@@ -69,7 +70,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName
+  const customListName = _.capitalize(req.params.customListName)
   List.findOne({name: customListName}, function(err, foundList){
     if(err){
     }
@@ -92,26 +93,58 @@ app.get("/:customListName", function(req, res){
 app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
+  const listName= req.body.list;
 
   const item = new Item({
     name: itemName
   })
 
-  item.save()
-  res.redirect("/")
+  if(listName === "Today"){
+    item.save()
+    res.redirect("/")
+  }
+  else{
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save()
+      res.redirect("/" + listName)
+    })
+  }
+
+ 
 })
 
 app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if(err){
-      console.log(err)
-    }
-    else{
-      console.log("Successfully deleted checked item")
-      res.redirect("/")
-    }
-  })
+  const listName = req.body.listName;
+
+  if(listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log("Successfully deleted checked item")
+        res.redirect("/")
+      }
+    })
+  }
+  else{
+    List.findOneAndUpdate(
+      {name: listName},
+      {$pull: {items: {_id: checkedItemId}}},
+      function(err, foundList){
+        if(err){
+
+        }
+        else{
+          res.redirect("/" + listName)
+        }
+      }
+    )
+  }
+
+  
 })
 
 app.get("/work", function(req,res){
